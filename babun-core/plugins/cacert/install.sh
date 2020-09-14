4cf667b255ecc9b507d441950c97166b35dac847
Kamil Mętrak <kamil.metrak@gmail.com>
2020-09-14 13:40:23 +0200

    Fix for not existing dir

--- babun-core/plugins/cacert/install.sh
+++ babun-core/plugins/cacert/install.sh
@@ -3,8 +3,9 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
+mkdir -p /usr/ssl/certs
 cd /usr/ssl/certs
-curl http://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
+curl https://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
 #c_rehash
 
 #possible problem with c_rehash in newest cygwin
