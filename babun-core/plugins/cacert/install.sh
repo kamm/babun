52acb63d4801aaaa4c1d5234f2fa55d28f9d0ffa
Kamil Mętrak <kamil.metrak@gmail.com>
2020-09-14 17:17:22 +0200

    fix install of plugin cacert

--- babun-core/plugins/cacert/install.sh
+++ babun-core/plugins/cacert/install.sh
@@ -3,10 +3,4 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
-mkdir -p /usr/ssl/certs
-cd /usr/ssl/certs
-curl https://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
-#c_rehash
-
-#possible problem with c_rehash in newest cygwin
-#for file in *.pem; do ln -s $file `openssl x509 -hash -noout -in $file`.0; done
+curl -k https://curl.haxx.se/ca/cacert.pem > /etc/pki/tls/certs/ca-bundle.crt
