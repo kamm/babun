173b9e05fedffa6546242a28ad09a341d84f8395
Kamil Mętrak <kamil.metrak@gmail.com>
2020-09-15 08:25:30 +0200

    Revert "fix install of plugin cacert"
    This reverts commit 52acb63d4801aaaa4c1d5234f2fa55d28f9d0ffa.

--- babun-core/plugins/cacert/install.sh
+++ babun-core/plugins/cacert/install.sh
@@ -3,4 +3,10 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
-curl -k https://curl.haxx.se/ca/cacert.pem > /etc/pki/tls/certs/ca-bundle.crt
+mkdir -p /usr/ssl/certs
+cd /usr/ssl/certs
+curl https://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
+#c_rehash
+
+#possible problem with c_rehash in newest cygwin
+#for file in *.pem; do ln -s $file `openssl x509 -hash -noout -in $file`.0; done
