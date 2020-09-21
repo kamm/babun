1b638680e40b0c44cb3501e197e755e28f7177ad
Kamil Mętrak <kamil.metrak@gmail.com>
2020-09-22 00:51:38 +0200

    Update install_home.sh

--- babun-core/plugins/dotfiles/install_home.sh
+++ babun-core/plugins/dotfiles/install_home.sh
@@ -2,10 +2,8 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
-#echo "ForwardX11Trusted yes" >> "$homedir/.ssh/config"
+src="$babun_plugins/dotfiles/src"
 
-
-src="$babun_source/babun-core/plugins/dotfiles/src"
-/bin/cp -rf "$src/minttyrc" "$home/.minttyrc"
-mkdir -p "$home/.mintty/emojis"
-tar -xf "$src/apple.tar" -C "$home/.mintty/emojis"
+/bin/cp -rf "$src/minttyrc" "$homedir/.minttyrc"
+mkdir -p "$homedir/.mintty/emojis"
+tar -xf "$src/apple.tar" -C "$homedir/.mintty/emojis"
