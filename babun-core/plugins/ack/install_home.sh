3394e8375695124d86c73d5467b9c0faaf2da262
Kamil Metrak <kamil.metrak@gmail.com>
2022-01-21 23:55:42 +0100

    fix ack plugin

--- babun-core/plugins/ack/install_home.sh
+++ babun-core/plugins/ack/install_home.sh
@@ -2,4 +2,5 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
+mkdir "$homedir/.vim"
 tar -C "$homedir/.vim" -xf "$babun_plugins/ack/src/ack-vim/ack.tar" 
