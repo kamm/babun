36b455b55258b9c8a8f2570fe13a9f3e7791dd58
Kamil Metrak <kamil.metrak@gmail.com>
2022-01-22 11:37:16 +0100

    fix dotfiles plugin

--- babun-core/plugins/dotfiles/install_home.sh
+++ babun-core/plugins/dotfiles/install_home.sh
@@ -5,22 +5,22 @@ source "$babun_tools/script.sh"
 src="$babun_plugins/dotfiles/src"
 
 mkdir -p "$homedir/.local/bin"
+mkdir -p "$homedir/.cache/vim/undodir"
+mkdir -p "$homedir/.mintty/emojis"
+mkdir -p "$homedir/.config/tmux"
+
+tar -xf "$src/apple.tar" -C "$homedir/.mintty/emojis"
 
 /bin/cp -rf "$src/minttyrc" "$homedir/.minttyrc"
-git clone https://github.com/morhetz/gruvbox.git "$homedir/.vim/pack/default/start/gruvbox"
 /bin/cp -rf "$src/vimrc" "$homedir/.vimrc"
-mkdir -p "$homedir/.mintty/emojis"
-tar -xf "$src/apple.tar" -C "$homedir/.mintty/emojis"
 /bin/cp "$src/battery" "$homedir/.local/bin"
 /bin/cp "$src/temperature" "$homedir/.local/bin"
-
 /bin/cp "$src/tm" "$homedir/.local/bin"
 /bin/cp "$src/tp" "$homedir/.local/bin"
-
-mkdir -p "$homedir/.config/tmux"
 /bin/cp "$src/tmux/tmux.conf.symlink" "$homedir/.config/tmux"
 /bin/cp "$src/tmux/base16.sh" "$homedir/.config/tmux"
 /bin/cp "$src/tmux/theme.sh" "$homedir/.config/tmux"
+
 ln -s "$homedir/.config/tmux/tmux.conf.symlink" "$homedir/.tmux.conf"
 
 chmod ugo+x "$homedir/.local/bin/battery"
