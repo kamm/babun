017ec790110f1941aeaba9ef77cacfbf5137bd5b
Kamil Metrak <kamil.metrak@gmail.com>
2022-01-25 21:28:42 +0100

    Remove unused things from shell plugin

--- babun-core/plugins/shell/install.sh
+++ babun-core/plugins/shell/install.sh
@@ -6,10 +6,10 @@ source "$babun_tools/script.sh"
 src="$babun_source/babun-core/plugins/shell/src/"
 dest="$babun/home/shell/"
 
-/bin/cp -rf /etc/minttyrc /etc/minttyrc.old  || echo ""
+/bin/cp -rf /etc/minttyrc /etc/minttyrc.old  2>/dev/null || true
 /bin/cp -rf $src/minttyrc /etc/minttyrc
 
-/bin/cp -rf /etc/zprofile /etc/zprofile.old || echo ""
+/bin/cp -rf /etc/zprofile /etc/zprofile.old 2>/dev/null || true
 /bin/cp -rf $src/zprofile /etc/zprofile
 
 mkdir -p "$dest"
--- babun-core/plugins/shell/install_home.sh
+++ babun-core/plugins/shell/install_home.sh
@@ -5,13 +5,3 @@ source "$babun_tools/script.sh"
 
 src="$babun/home/shell/"
 
-## if vim not installed
-#if [[ ! -d "$homedir/.vim" ]]; then
-#	/bin/cp -rf "$src/.vim" "$homedir/.vim"	
-#	tar -C "$homedir/.vim" -xf "$src/.vim/colors.tar" 
-#fi
-
-#if [[ ! -f "$homedir/.minttyrc" ]]; then
-#	touch "$homedir/.minttyrc"
-#fi
-
