1e2f65dad3e5da97c5122d6e4478961200bce4ae
Kamil Metrak <kamil.metrak@gmail.com>
2022-01-26 18:37:31 +0100

    Fix for building both x86 and x86_64 versions

--- babun-core/tools/init.sh
+++ babun-core/tools/init.sh
@@ -3,3 +3,7 @@ set -e -f -o pipefail
 
 # init babun.instance before anything other initializes
 /bin/cp -rf /usr/local/etc/babun/source/babun-core/plugins/core/src/babun.instance /usr/local/etc
+
+if [ ! "x${1}" = "x" ]; then
+    echo "${1} > /usr/local/etc/cygwinBitVersion
+fi
