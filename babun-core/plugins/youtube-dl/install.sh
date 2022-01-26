0e158a88b32ff33fbfbf2afe8a60024dfb4d34ab
Kamil Metrak <kamil.metrak@gmail.com>
2022-01-26 13:46:44 +0100

    fix youtube-dl plugin

--- babun-core/plugins/youtube-dl/install.sh
+++ babun-core/plugins/youtube-dl/install.sh
@@ -2,13 +2,13 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
-curl https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe  > /usr/local/bin/yt-dlp.exe 2>/dev/null
-curl https://yt-dl.org/downloads/2021.12.17/youtube-dl.exe > /usr/local/bin/youtube-dl.exe 2>/dev/null
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe  > /usr/local/bin/yt-dlp.exe 2>/dev/null
+curl -L https://yt-dl.org/downloads/2021.12.17/youtube-dl.exe > /usr/local/bin/youtube-dl.exe 2>/dev/null
 
 chmod 755 /usr/local/bin/yt-dlp.exe
 chmod 755 /usr/local/bin/youtube-dl.exe
 ffmpeg="ffmpeg-5.0-essentials_build.zip"
-curl https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.0-essentials_build.zip > /tmp/${ffmpeg} 2>/dev/null
+curl -L https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.0-essentials_build.zip > /tmp/${ffmpeg} 2>/dev/null
 
 unzip -j /tmp/${ffmpeg} ffmpeg-5.0-essentials_build/bin/ffmpeg.exe >/dev/null 2>&1
 unzip -j /tmp/${ffmpeg} ffmpeg-5.0-essentials_build/bin/ffprobe.exe >/dev/null 2>&1
